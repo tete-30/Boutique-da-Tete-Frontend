b1ff9ab3e87f6bb89e36f2977a4b1bc556797184
tete-30 <112039750+tete-30@users.noreply.github.com>
2024-01-10 15:58:10 -0300

    teste de login, cadastro e banco

--- src/pages/cadastro/cadastroscript.js
+++ src/pages/cadastro/cadastroscript.js
@@ -1,64 +1,7 @@
-function cadastrar() {
-    const nome = document.getElementById("nome").value;
-    const cpf = document.getElementById("cpf").value;
-    const endereco = document.getElementById("endereco").value;
-    const numero = document.getElementById("numero").value;
-    const complemento = document.getElementById("complemento").value;
-    const cep = document.getElementById("cep").value;
-    const bairro = document.getElementById("bairro").value;
-    const cidade = document.getElementById("cidade").value;
-    const uf = document.getElementById("uf").value;
-    const telefone = document.getElementById("telefone").value;
-    const celular = document.getElementById("celular").value;
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-    const confirmeSenha = document.getElementById("confirmeSenha").value;
 
-    if (validaCadastro()) {
-        resultadoSection.innerHTML = "<p>Entrando...</p>";
 
-        const dadosCadastro = {
-            nome: nome,
-            cpf: cpf,
-            endereco: endereco,
-            numero: numero,
-            complemento: complemento,
-            cep: cep,
-            bairro: bairro,
-            cidade: cidade,
-            uf: uf,
-            telefone: telefone,
-            celular: celular,
-            email: email,
-            senha: senha,
-            confirmeSenha: confirmeSenha,
-        };
 
-        axios.post('http://localhost:3000/cadastro', dadosCadastro, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(response => {
-            console.log(response.data);
-        
-            if (response.data && response.data.success) {
-                resultadoSection.innerHTML = "<p>Cadastro realizado com sucesso!</p>";
-            } else {
-                resultadoSection.innerHTML = "<p>Erro ao cadastrar. Tente novamente.</p>";
-            }
-        })
-        .catch(error => {
-            console.error(error);
-        
-            if (error.response && error.response.status === 422) {
-                resultadoSection.innerHTML = "<p>Erro de validação. Por favor, verifique os campos e tente novamente.</p>";
-            } else {
-                resultadoSection.innerHTML = "<p>Erro desconhecido. Tente novamente mais tarde.</p>";
-            }
-        });
-    }}
-        
+ 
 
 function validaCadastro() {
  
@@ -94,3 +37,85 @@ function validaSenha(senha) {
     return isValid;
 }
     
+function cadastro() {
+    const resultadoSection = document.getElementById("resultadoSection");
+    
+    if (!resultadoSection) {
+        console.error("Elemento com ID 'resultadoSection' não encontrado.");
+        return;
+    }
+    const nome = document.getElementById("nome").value;
+    const cpf = document.getElementById("cpf").value;
+    const endereco = document.getElementById("endereco").value;
+    const numero = document.getElementById("numero").value;
+    const complemento = document.getElementById("complemento").value;
+    const cep = document.getElementById("cep").value;
+    const bairro = document.getElementById("bairro").value;
+    const cidade = document.getElementById("cidade").value;
+    const uf = document.getElementById("uf").value;
+    const telefone = document.getElementById("telefone").value;
+    const celular = document.getElementById("celular").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const confirmeSenha = document.getElementById("confirmeSenha").value;
+
+    resultadoSection.innerHTML = `
+    <p>Nome: ${nome}</p>
+    <p>CPF: ${cpf}</p>
+    <p>endereco: ${endereco}</p>
+    <p>numero: ${numero}</p>
+    <p>complemento: ${complemento}</p>
+    <p>cep: ${cep}</p>
+    <p>bairro: ${bairro}</p>
+    <p>cidade: ${cidade}</p>
+    <p>uf: ${uf}</p>
+    <p>telefone: ${telefone}</p>
+    <p>celular: ${celular}</p>
+    <p>email: ${email}</p>
+    <p>senha: ${senha}</p>
+    <p>confirmeSenha: ${confirmeSenha}</p>
+    `;
+
+    if (validaCadastro()) {
+        resultadoSection.innerHTML = "<p>Entrando...</p>";
+
+        const dadosCadastro = {
+            nome: nome,
+            cpf: cpf,
+            endereco: endereco,
+            numero: numero,
+            complemento: complemento,
+            cep: cep,
+            bairro: bairro,
+            cidade: cidade,
+            uf: uf,
+            telefone: telefone,
+            celular: celular,
+            email: email,
+            senha: senha,
+            confirmeSenha: confirmeSenha,
+        };
+
+        axios.post('http://localhost:3000/cadastro', dadosCadastro, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            console.log(response.data);
+        
+            if (response.data && response.data.success) {
+                resultadoSection.innerHTML = "<p>Cadastro realizado com sucesso!</p>";
+            } 
+        })
+        .catch(error => {
+            console.error(error);
+        
+            if (error.response && error.response.status === 422) {
+                resultadoSection.innerHTML = "<p>Erro de validação. Por favor, verifique os campos e tente novamente.</p>";
+            } else {
+                resultadoSection.innerHTML = "<p>Erro desconhecido. Tente novamente mais tarde.</p>";
+            }
+        });
+    }}
+       
--- src/pages/login/script.js
+++ src/pages/login/script.js
@@ -1,32 +1,57 @@
+
+
+function validaEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validaSenha(senha) {
+    return senha.length >= 6; 
+}
+
 function exibirMensagem() {
+    const resultadoSection = document.getElementById("resultadoSection");
+    
+    if (!resultadoSection) {
+        console.error("Elemento com ID 'resultadoSection' não encontrado.");
+        return;
+    }
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
 
-    // Validar e-mail e senha (adapte conforme necessário)
+    resultadoSection.innerHTML = `
+    <p>email: ${email}</p>
+    <p>senha: ${senha}</p>
+    
+    `;
     var emailValido = validaEmail(email);
     var senhaValida = validaSenha(senha);
 
     if (emailValido && senhaValida) {
         resultadoSection.innerHTML = "<p>Entrando...</p>";
+        const dadosLogin = {
+            email: email,
+            senha: senha
+        };
+
+        axios.post('http://localhost:3000/login', dadosLogin, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            console.log(response.data);
+
+            if (response.data && response.data.message === "Login efetuado com sucesso!") {
+                resultadoSection.innerHTML = "<p>Login efetuado com sucesso!</p>";
+            } 
+        })
+        .catch(error => {
+            console.error(error);
+            resultadoSection.innerHTML = "<p>Erro ao fazer login. Tente novamente mais tarde.</p>";
+        });
+
     } else {
         resultadoSection.innerHTML = '<p>Credenciais inválidas. Por favor, verifique e tente novamente.</p>';
     }
 }
-
-function validaEmail(email) {
-    // Adicione lógica de validação de e-mail aqui
-    // Retorne true se o e-mail for válido, false caso contrário
-    return true;
-}
-
-function validaSenha(senha) {
-    // Adicione lógica de validação de senha aqui
-    // Retorne true se a senha for válida, false caso contrário
-    return true;
-}
-
-const dadosLogin = {
-    email: email,
-    senha: senha
-};
-
